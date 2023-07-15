5b591c284a5e27362c013b26231f6c56f392066b
Enio <eniobatalha@gmail.com>
2023-07-15 18:01:06 -0300

    Front-End ExibitReceita 90%

--- ClientSide/src/Pages/PaginaExibirReceita/BodyExibirReceita.tsx
+++ ClientSide/src/Pages/PaginaExibirReceita/BodyExibirReceita.tsx
@@ -1,42 +1,49 @@
 import CriarHeader from "../../components/Header/Header";
 import styles from "./BodyExibirReceita.module.scss"
 
-export default function CriarBodyExibirReceita(){
+export default function CriarBodyExibirReceita() {
 
-    return(
+    return (
         <div className={styles.pagina}>
-            <CriarHeader/>
+            <CriarHeader />
             <div className={styles.receita}>
-                <div className={styles.top}>
+                <div className={styles.left}>
                     <div className={styles.imagem}>
-                        <img src="#" alt="imagem-receita" />
+                        <img src="https://fyoti.com.br/wp-content/uploads/2022/08/Receita-de-Bolo-de-chocolate-decorado.jpg" alt="imagem-receita" />
                     </div>
-                    <div className={styles.textoReceita}>
-                        <h2>Titulo Receita</h2>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt ipsam nostrum fugit iure temporibus. Repellendus accusantium praesentium, ratione et atque corrupti rem, recusandae enim aut expedita eveniet obcaecati maxime aliquam?
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis cum enim odio? Eaque eos, ipsum temporibus animi aspernatur modi reiciendis dolorum natus itaque facilis pariatur nisi dignissimos aliquam expedita culpa.
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.bot}>
                     <div className={styles.ingredientes}>
                         <ul>
-                            <li>arroba</li>
-                            <li>arroba</li>
-                            <li>arroba</li>
-                            <li>arroba</li>
-                            <li>arroba</li>
-                            <li>arroba</li>
+                            <li>2 xícaras de farinha de trigo</li>
+                            <li>1 xícara de cacau em pó</li>
+                            <li>2 xícaras de açúcar</li>
+                            <li>2 colheres de chá de fermento em pó</li>
+                            <li>1 colher de chá de bicarbonato de sódio</li>
+                            <li>1/2 colher de chá de sal</li>
+                            <li>2 ovos grandes</li>
+                            <li>1 xícara de leite</li>
                         </ul>
                     </div>
-                    <div className={styles.modoPreparo}>
-                        <ul>
-                            <li>preparo</li>
-                            <li>preparo</li>
-                            <li>preparo</li>
-                            <li>preparo</li>
-                            <li>preparo</li>
-                        </ul>
+                </div>
+                <div className={styles.right}>
+                    <div className={styles.textoReceita}>
+                        <h2>Bolo de Chocolate com Morangos</h2>
+                        <p>Prepare-se para se deliciar com um incrível bolo de chocolate com cobertura de ganache e morangos frescos. Esta receita é perfeita para aqueles momentos em que você deseja surpreender seus convidados com uma sobremesa irresistível. O bolo macio e úmido, combinado com a rica e sedosa ganache de chocolate e os morangos frescos, resultam em uma combinação de sabores e texturas que irão encantar seu paladar.
+                        </p>
+
+                        <p><b>Modo de preparo:</b></p>
+
+                        <p>
+                        1. Pré-aqueça o forno a 180°C e unte uma forma redonda com manteiga e farinha de trigo. 
+                        2. Em uma tigela grande, peneire 2 xícaras de farinha de trigo, 1 xícara de cacau em pó, 2 xícaras de açúcar, 2 colheres de chá de fermento em pó e 1 colher de chá de bicarbonato de sódio. Misture bem os ingredientes secos. <br/>
+                        3. Em outra tigela, adicione 2 ovos, 1 xícara de leite e 1/2 xícara de óleo vegetal. Bata os ingredientes líquidos até obter uma mistura homogênea. <br/>
+                        4. Despeje a massa na forma previamente preparada, nivelando a superfície com uma espátula. <br/>
+                        5. Leve ao forno pré-aquecido e asse por aproximadamente 30 a 35 minutos, ou até que um palito inserido no centro do bolo saia limpo. <br/>
+                        6. Retire o bolo do forno e deixe esfriar na forma por alguns minutos. Em seguida, transfira para uma grade de resfriamento e deixe esfriar completamente. <br/>
+                        7. Enquanto o bolo esfria, você pode preparar uma cobertura de sua preferência, como ganache de chocolate, buttercream ou brigadeiro. <br/>
+                        8. Após o bolo estar completamente frio, despeje a cobertura escolhida sobre o bolo, espalhando uniformemente com uma espátula. <br/>
+                        9. Decore o bolo de chocolate com raspas de chocolate, morangos frescos, confeitos ou o que preferir. <br/>
+                        </p>
+                                      
                     </div>
                 </div>
             </div>
